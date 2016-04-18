82bd61c33a0dc49fbbcab2ab172ecf83c1bd6fd9
Joao Cardoso <jp.penixe@gmail.com>
2016-04-18 17:25:46 +0200

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -23,7 +23,7 @@ if(isset($_GET['e2'])) {
     echo "#NAME FOS-Streaming \r\n";
     foreach($user->categories as $category) {
         $streamord = $category->streams;
-        usort($streamord, 'sortByOrder');
+        $streamord = usort($streamord, 'sortByOrder');
         foreach($streamord as $stream) {
             if($stream->running == 1) {
                 echo "#SERVICE 1:0:1:0:0:0:0:0:0:0:http%3A//".$setting->webip."%3A".$setting->webport."/live/".$user->username."/".$user->password."/".$stream->id ."\r\n";
