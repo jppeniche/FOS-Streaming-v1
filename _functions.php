73268d356d42d1664927f842f3a16648211758fe
Joao Cardoso <jp.penixe@gmail.com>
2016-04-18 17:10:14 +0200

    Update _functions.php

--- _functions.php
+++ _functions.php
@@ -342,4 +342,5 @@ function array_sort_by_column(&$arr, $col, $dir = SORT_ASC) {
     foreach ($arr as $key=> $row) {
         $sort_col[$key] = $row[$col];
     }
+    return array_multisort($sort_col, $dir, $arr);
 }
