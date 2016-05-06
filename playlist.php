69b126ebf13c2cf4974b02ed98dfb99b1926d071
Joao Cardoso <jp.penixe@gmail.com>
2016-05-06 12:05:59 +0200

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -45,7 +45,7 @@ if(isset($_GET['m3u'])) {
         foreach($category->streams as $stream) {
         	if($print_cat) {
                		$cat = $stream->category["name"];
-               		echo "#EXTINF:0, ###" . $cat . "\r\n";
+               		echo "#EXTINF:0, ###" . $cat . "###\r\n";
                		echo "http://0.0.0.0/999.ts"."\r\n";
                		$print_cat=False;                                                                
 	        }           
