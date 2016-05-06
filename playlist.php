ccee800f89c7015104ee968592534a9a9a2ed861
Joao Cardoso <jp.penixe@gmail.com>
2016-05-06 11:58:05 +0200

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -43,6 +43,12 @@ if(isset($_GET['m3u'])) {
     foreach($user->categories as $category) {
  	$print_cat=True;
         foreach($category->streams as $stream) {
+        	if($print_cat) {
+               		$cat = $stream->category["name"];
+               		echo "#EXTINF:0, ###" . $cat . "\r\n";
+               		echo "http://0.0.0.0/999.ts"."\r\n";
+               		$print_cat=False;                                                                
+	        }           
   		if($stream->running == 1) {
                 	if (strlen(strstr($agent, 'Kodi')) > 0) {	
 		        	echo "#EXTINF:0 tvg-logo=\"" . $stream->logo . "\" tvg-id=\"" . $stream->tvid . "\" ,[COLOR green]" . $stream->name . "[/COLOR]\r\n";
